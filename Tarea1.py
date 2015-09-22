9ac54606f04dea23f6b7db5d5eeedc18d804b315
maxidirk <maxidirk@yahoo.es>
2015-09-22 20:34:13 -0300

    se escribio la integral de forma mas compacta

--- Tarea1.py
+++ Tarea1.py
@@ -39,18 +39,24 @@ plt.show()
 #Nota: El flujo es la integral de la Potencia en todo lambda (?)
 #######################################################
 #Parte de calcular la luminosidad
+'''
+Integral usando metodo del trapecio
+int f(x) dx = dx/2 * (f(xo) + f(xo + dx))
+en lo que sigue w1 es x y e1 es f(x)
+'''
+Ft = 0   #Flujo total a obtener
 
-#integral usando metodo del trapecio
-#int f(x) dx = dx/2 * (f(xo) + f(xo + dx))
-I = 0   #valor de la integral que se obtendra
 for i in range(n-1):
-    f1 = e1[i]
-    f2 = e1[i+1]
-    x1 = w1[i]
-    x2 = w1[i+1]
-    I += ( (x2-x1) / 2.) * (f1 + f2)
+    Ft += ( (w1[i+1]-w1[i]) / 2.) * (e1[i] + e1[i+1])
 
-print I
+print Ft #en cgs
+
+
+
+
+
+
+
 #######################################################
 #Parte funcion de Plank
 
