2d81b650d19cfdb478e7ed1feac484630c104232
maxidirk <maxidirk@yahoo.es>
2015-09-21 23:44:01 -0300

    Se crean las variables w y e obtenidas de los datos

--- Tarea1.py
+++ Tarea1.py
@@ -1,22 +1,26 @@
-#Tarea01
-#Maximiliano Dirk Vega Aguilera 18.451.231-9
-
+'''
+Metodos Numericos para la Ciencia e Ingenieria FI3104
+Tarea01
+Maximiliano Dirk Vega Aguilera
+18.451.231-9
+'''
 #######################################################
 
 import numpy as np
-#import matplotlib.pyplot as plt
-from pylab import *
+import matplotlib.pyplot as plt
 import astropy as ast
 import scipy as sci
 
 #######################################################
 
-a=np.loadtxt('sun_AM0.dat')
+a=np.loadtxt('sun_AM0.dat')  # Se abre el archivo
+
+n = len(a)
+w = np.zeros(n)     # w = Wavelength (nm)
+e = np.zeros(n)     # e = Power (W*m-2*nm-1)
+w = a[:,0]
+e = a[:,1]
 
 
-
-plot(log(a[0][:]),log(a[1][:]))
-show()
-
-	
-
+print w
+print e
