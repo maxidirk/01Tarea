e7e87d734790ff5e6a3f167fe031813aaf063a10
maxidirk <maxidirk@yahoo.es>
2015-09-22 01:32:08 -0300

    se agrego una nota, nada importante

--- Tarea1.py
+++ Tarea1.py
@@ -26,8 +26,24 @@ w1 = 10. * w          # w1 = Wavelength (Angstrom)
 e1 = e * ((10 ** (7)) * (10. ** (-4.)) * (10 ** (-1)))  # e1 = Power (erg*s-1*cm-2*A-1)
 
 plt.plot(np.log(w1),np.log(e1))
+plt.plot(np.log10(w1),np.log10(e1))
+plt.title('$\lambda$ vs Potencia en escala Log-Log')
 plt.xlabel('Longitud de Onda ($\lambda$) $[\AA]$')
-plt.ylabel('Potencia $[erg \ s^{-1} cm^{-2} \AA^{-1}]$')
+plt.ylabel('Potencia  $[erg \ s^{-1} cm^{-2} \AA^{-1}]$')
 plt.show()
+
+#Nota: El flujo es la integral de la Potencia en todo lambda
+#######################################################
+#Parte de calcular la luminosidad
+
+
+
+
+#######################################################
+#Parte funcion de Plank
+
+
+
+
 
 #######################################################
